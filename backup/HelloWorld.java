008a5e4ed44f2ce16abd5d6b78dcf2d4cfc73275
skondla <cloudarchitectusa@gmail.com>
2019-10-05 12:15:52 -0400

    Updated HelloWorld Java program

--- backup/HelloWorld.java
+++ backup/HelloWorld.java
@@ -16,8 +16,7 @@ public class HelloWorld {
 		// TODO Auto-generated method stub
 		int i=0;
 		while (i <= 100) {
-			//if (i <=50) {
-			if ((i % 2) == 0){
+			if (i <=50) {
 				System.out.println(i + ": Hello Dear");
 			}
 			else {
--- myworld/datacube/com/HelloWorld.java
+++ myworld/datacube/com/HelloWorld.java
@@ -16,7 +16,8 @@ public class HelloWorld {
 		// TODO Auto-generated method stub
 		int i=0;
 		while (i <= 100) {
-			if (i <=50) {
+			//if (i <=50) {
+			if ((i % 2) == 0){
 				System.out.println(i + ": Hello Dear");
 			}
 			else {
