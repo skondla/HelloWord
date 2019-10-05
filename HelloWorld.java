ff907aa9b5b7bc7e96782bf754629f3071811e77
skondla <cloudarchitectusa@gmail.com>
2019-10-05 11:48:41 -0400

    Added HelloWorld Java Program for Jenkin Integration

---
+++ HelloWorld.java
@@ -0,0 +1,29 @@
+/**
+ * 
+ */
+package myworld.datacube.com;
+
+/**
+ * @author skondla
+ *
+ */
+public class HelloWorld {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int i=0;
+		while (i <= 100) {
+			if (i <=50) {
+				System.out.println(i + ": Hello Dear");
+			}
+			else {
+				System.out.println(i + ": No Dear");
+			}
+			i++;
+		}
+	}
+}
+
